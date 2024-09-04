c6a8ec7542a3d3018f1eaf2529629f33025a4e5b
rahimyavuzturan <turanrahimyavuz@gmail.com>
2024-09-04 22:09:08 +0300

    initial stateler eklendi

--- snake_game.py
+++ snake_game.py
@@ -1,3 +1,56 @@
 import pygame
 import random
+from enum import Enum
+from collections import namedtuple
 
+pygame.init()
+
+Point = namedtuple('Point', 'x', 'y')
+
+BLOCK_SIZE = 20
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+class SnakeGame():
+    # en ve boy
+    def __init__(self, w = 640, h=480):
+        self.w = w
+        self.h = h
+    
+        # pygame ekranı ayarlama
+        self.display = pygame.display.set_mode((self.w,self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+
+        #başlangıç durumları
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2 , self.h/2)
+        self.snake = [self.head,
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+        
+
+
+    def play_step(self):
+        pass
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # loop
+    while True:
+        game.play_step()
+
+        #oyun bitişi kontrolü
+
+    pygame.quit()
